e79c51f3e42640c3fb3e9da16e3f50971b313954
Nahue507 <37816787+Nahue507@users.noreply.github.com>
2022-04-05 19:30:07 -0300

    First Commit

---
+++ CryptoP2P/src/main/java/ar/edu/unq/CryptoP2P/CryptoP2PApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.CryptoP2P;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoP2PApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoP2PApplication.class, args);
+	}
+
+}
---
+++ CryptoP2P/src/test/java/ar/edu/unq/CryptoP2P/CryptoP2PApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.CryptoP2P;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoP2PApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
