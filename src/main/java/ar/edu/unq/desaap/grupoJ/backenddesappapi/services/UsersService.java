d713e167b0eddc0d885e218599923a2c503e1d07
Nahue507 <nahuelggt@gmail.com>
2022-04-27 21:47:31 -0300

    Users : Changes in exceptions

--- src/main/java/ar/edu/unq/desaap/grupoJ/backenddesappapi/services/UsersService.java
+++ src/main/java/ar/edu/unq/desaap/grupoJ/backenddesappapi/services/UsersService.java
@@ -2,6 +2,8 @@ package ar.edu.unq.desaap.grupoJ.backenddesappapi.services;
 
 import ar.edu.unq.desaap.grupoJ.backenddesappapi.model.User;
 import ar.edu.unq.desaap.grupoJ.backenddesappapi.repositories.IUserRepository;
+import ar.edu.unq.desaap.grupoJ.backenddesappapi.services.Exceptions.UsersException;
+import org.omg.CORBA.UserException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +13,10 @@ public class UsersService {
     @Autowired
     private IUserRepository userRepository;
 
-    public User save(User newUser) {
+    public User save(User newUser) throws UsersException {
+        if(!newUser.isValidCVU()){
+            throw new UsersException("Invalid CVU length");
+        }
         return userRepository.save(newUser);
     }
 }
--- src/main/java/ar/edu/unq/desaap/grupoJ/backenddesappapi/webServices/UsersController.java
+++ src/main/java/ar/edu/unq/desaap/grupoJ/backenddesappapi/webServices/UsersController.java
@@ -3,6 +3,7 @@ package ar.edu.unq.desaap.grupoJ.backenddesappapi.webServices;
 import ar.edu.unq.desaap.grupoJ.backenddesappapi.model.User;
 import ar.edu.unq.desaap.grupoJ.backenddesappapi.services.Exceptions.UsersException;
 import ar.edu.unq.desaap.grupoJ.backenddesappapi.services.UsersService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,12 +24,18 @@ public class UsersController {
     @PostMapping(path = "users",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<User> Create(@RequestBody User newUser) throws UsersException {
+    public ResponseEntity<User> Create(@RequestBody User newUser) throws Exception {
+
         try {
             User user = usersService.save(newUser);
             return new ResponseEntity<>(user, HttpStatus.CREATED);
-        } catch (Exception e) {
-            throw new UsersException("User could not be created");
+
+        }
+        catch (UsersException e){
+            throw new Exception(e.getMessage());
+        }
+        catch (Exception e) {
+            throw new Exception("User could not be created");
         }
     }
 }
