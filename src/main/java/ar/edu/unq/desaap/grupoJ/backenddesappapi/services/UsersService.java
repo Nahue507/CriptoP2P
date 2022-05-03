24e205d0d41a7fe8866d65349685715689b2873a
Martin Serna <mserna@controlunion.com>
2022-05-03 19:10:31 -0300

    UsersService: import error fix

--- src/main/java/ar/edu/unq/desaap/grupoJ/backenddesappapi/services/UsersService.java
+++ src/main/java/ar/edu/unq/desaap/grupoJ/backenddesappapi/services/UsersService.java
@@ -3,7 +3,6 @@ package ar.edu.unq.desaap.grupoJ.backenddesappapi.services;
 import ar.edu.unq.desaap.grupoJ.backenddesappapi.model.User;
 import ar.edu.unq.desaap.grupoJ.backenddesappapi.repositories.IUserRepository;
 import ar.edu.unq.desaap.grupoJ.backenddesappapi.services.Exceptions.UsersException;
-import org.omg.CORBA.UserException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
